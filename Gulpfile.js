7afe4df924b7c0f4d43fbacb2415fc827cb8b70a
Steve Ayers <smaye81@gmail.com>
2014-09-07 23:15:47 -0400

    Removed code

--- Gulpfile.js
+++ Gulpfile.js
@@ -18,39 +18,3 @@ gulp.task('watch', function () {
 
     return rebundle();
 });
-
-
-//var gulp = require('gulp');
-//var gutil = require('gulp-util');
-//var browserify = require('browserify');
-//var watchify = require('watchify');
-//var vinylSource = require('vinyl-source-stream');
-//
-//gulp.task('browserify', function() {
-//    return browserify('./www/js/app.js')
-//        .bundle()
-//        .pipe(vinylSource('bundle.js'))
-//        .pipe(gulp.dest('./www/js'));
-//});
-//
-//gulp.task('watchify', function() {
-//    var watchified = watchify('./www/js/app.js');
-//
-//    function bundleWatched() {
-//        return watchified
-//            .bundle({debug: true})
-//            .pipe(vinylSource('bundle.js'))
-//            .pipe(gulp.dest('./www/js'));
-//    }
-//
-//    watchified.on('update', bundleWatched);
-//    watchified.on('time', function(time) {
-//        gutil.log('Watchify', gutil.colors.cyan("'default'"), 'after', gutil.colors.magenta(time), gutil.colors.magenta('ms'));
-//    });
-//
-//    return bundleWatched();
-//});
-//
-//gulp.task('default', ['watchify']);
-//
-//gulp.task('build', ['browserify']);
